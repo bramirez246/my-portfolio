3303f969109856e5d157866a1e116c2a1de03f3b
bramirez246 <bramirez246@gmail.com>
2024-02-15 14:36:30 -0800

    added NotFound page and error handling

---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page" className="p-5 text-center">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <p>Please go to <Link to="/">Homepage</Link></p>
+    </div>
+  );
+}
